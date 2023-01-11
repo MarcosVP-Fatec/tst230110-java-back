ff0bdf690dbab10673252b8f925467e79bdfed1b
MarcosVP <marcos.pereira47@fatec.sp.gov.br>
2023-01-11 00:40:46 -0300

    Inicial

---
+++ src/main/java/br/com/marcosvp/tst230110javaback/Tst230110JavaBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcosvp.tst230110javaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tst230110JavaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tst230110JavaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcosvp/tst230110javaback/Tst230110JavaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcosvp.tst230110javaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tst230110JavaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
